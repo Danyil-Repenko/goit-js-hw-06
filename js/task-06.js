dd3f825ea6d80ad8014b60eac01864fc696ea3ac
Danyil-Repenko <danyil.oficial@gmail.com>
2022-07-28 10:54:19 +0300

    прибрав зайвий тернарник

--- js/task-06.js
+++ js/task-06.js
@@ -6,12 +6,12 @@ function onInputBlur(event) {
     const eventTurget = event.currentTarget;
     if (eventTurget.value.length < eventTurget.dataset.length
         || eventTurget.value.length > eventTurget.dataset.length) {
-        eventTurget.classList.contains('valid')
-            ? eventTurget.classList.replace('valid', 'invalid')
-            : eventTurget.classList.add('invalid');
+        if (eventTurget.classList.contains('valid')) {
+            eventTurget.classList.replace('valid', 'invalid');
+        } else { eventTurget.classList.add('invalid'); };
     } else {
-        eventTurget.classList.contains('invalid')
-            ? eventTurget.classList.replace('invalid', 'valid')
-            : eventTurget.classList.add('valid');
+        if (eventTurget.classList.contains('invalid')) {
+            eventTurget.classList.replace('invalid', 'valid');
+        } else { eventTurget.classList.add('valid'); };
     }
 }
